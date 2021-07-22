9a41b0631ef816db36344b8d2a1293fa88909854
HollyRob <85087775+HollyRob@users.noreply.github.com>
2021-07-22 16:42:48 +0930

    add inital files

---
+++ index.js
@@ -0,0 +1,21 @@
+//Random Number dice image generator//
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var images1 = "images/dice" + randomNumber1 + ".png";
+var images2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".dice .img1").setAttribute("src", images1);
+document.querySelector(".dice .img2").setAttribute("src", images2);
+
+
+//Winning player h1 generator//
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
